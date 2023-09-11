1fcfa803af3cbced998f2f8107bbe61a2eefe42e
deepthipraksh25 <deepthiprakash252@gmail.com>
2023-09-11 15:52:49 +0530

    first commit

---
+++ src/hello/world.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("We are trying to explore to Git");
+
+		System.out.println("We also explore what is local and Remote repo");
+
+		System.out.println("We also explore what is GitBash and Git Hub");
+	}
+
+}
